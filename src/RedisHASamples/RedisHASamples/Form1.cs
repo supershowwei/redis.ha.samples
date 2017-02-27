d5fdbc5d0a058f740ae0052d8f1cb32821072d0d
supershowwei <supershowwei@gmail.com>
2017-02-27 15:04:01 +0800

    complete getting master endpoint from redis-sentinel

---
+++ src/RedisHASamples/RedisHASamples/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using StackExchange.Redis;
+
+namespace RedisHASamples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = $"Master EndPoint: {this.GetCurrentMasterEndPoint()}";
+        }
+
+        private EndPoint GetCurrentMasterEndPoint()
+        {
+            var sentinelConfig = new ConfigurationOptions
+                                     {
+                                         ServiceName = "mymaster",
+                                         TieBreaker = string.Empty,
+                                         CommandMap = CommandMap.Sentinel,
+                                         DefaultVersion = new Version(3, 0),
+                                         AllowAdmin = true
+                                     };
+            sentinelConfig.EndPoints.Add("192.168.8.131", 26379);
+
+            var sentinelConnection = ConnectionMultiplexer.Connect(sentinelConfig);
+            var endPoint = sentinelConnection.GetEndPoints().First();
+            var server = sentinelConnection.GetServer(endPoint);
+
+            return server.SentinelGetMasterAddressByName("mymaster");
+        }
+    }
+}
