131421c140b7a271eb31b89d0ee0e9387f74ed93
supershowwei <supershowwei@gmail.com>
2017-02-27 16:21:02 +0800

    complete subscribe +switch-master channel

---
+++ src/RedisHASamples/RedisHASamples/ControlExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace RedisHASamples
+{
+    internal static class ControlExtension
+    {
+        public static void InvokeIfNecessary(this Control control, MethodInvoker action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
--- src/RedisHASamples/RedisHASamples/Form1.cs
+++ src/RedisHASamples/RedisHASamples/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using StackExchange.Redis;
 
@@ -35,6 +36,42 @@ namespace RedisHASamples
             var server = sentinelConnection.GetServer(endPoint);
 
             return server.SentinelGetMasterAddressByName("mymaster");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sentinelConfig = new ConfigurationOptions
+                                     {
+                                         ServiceName = "mymaster",
+                                         TieBreaker = string.Empty,
+                                         CommandMap = CommandMap.Sentinel,
+                                         DefaultVersion = new Version(3, 0),
+                                         AllowAdmin = true
+                                     };
+            sentinelConfig.EndPoints.Add("192.168.8.131", 26379);
+
+            var sentinelConnection = ConnectionMultiplexer.Connect(sentinelConfig);
+            var sentinelSubscriber = sentinelConnection.GetSubscriber();
+            sentinelSubscriber.Subscribe(
+                "+switch-master",
+                (channel, value) =>
+                    {
+                        var newMasterEndPoint = this.GetCurrentMasterEndPoint(value);
+
+                        this.textBox1.InvokeIfNecessary(
+                            () => { this.textBox1.Text = $"Subscribed Master EndPoint: {newMasterEndPoint}"; });
+                    });
+        }
+
+        private EndPoint GetCurrentMasterEndPoint(RedisValue value)
+        {
+            var regex = Regex.Match(
+                value.ToString(),
+                @"(?<masterName>.*)\s(?<fromHost>.*)\s(?<fromPort>.*)\s(?<toHost>.*)\s(?<toPort>.*)");
+
+            return new IPEndPoint(
+                       IPAddress.Parse(regex.Groups["toHost"].Value),
+                       int.Parse(regex.Groups["toPort"].Value));
         }
     }
 }
